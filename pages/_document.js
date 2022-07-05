8f735440ad6a49c9dfb391e45b4f36720221b1d5
Alexander <root@root>
2022-07-05 17:23:04 +0500

    add _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+       
+      </Head>
+
+      <body>
+      <Main />
+      <NextScript />
+      </body>
+    </Html>
+  )
+}
